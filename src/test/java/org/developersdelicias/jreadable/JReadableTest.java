c99731b3dd4fb04e94f1a0567568e0bb28eb56a7
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-08-12 08:08:45 -0500

    Add JUnit5 configuration and test

---
+++ src/test/java/org/developersdelicias/jreadable/JReadableTest.java
@@ -0,0 +1,14 @@
+package org.developersdelicias.jreadable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class JReadableTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
